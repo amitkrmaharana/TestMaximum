a68e84f24888b2d74b7642713daa4fd927be10b4
amitkrmaharana <ak657harley@gmail.com>
2021-02-27 20:37:04 +0530

    Added: added welcome message

---
+++ FindMaximum.java
@@ -0,0 +1,5 @@
+public class FindMaximum { // Find maximum number from the given list
+     public static void main(String[] args) {
+        System.out.println("Welcome to Test maximum Program");
+    }
+}
