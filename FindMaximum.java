97cb6866c530074eb96e503b0984045a36d3d0c0
amitkrmaharana <ak657harley@gmail.com>
2021-02-28 20:10:44 +0530

    Refactored the test file to include strings

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,12 +1,58 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class FindMaximum { // Find maximum value from the given list
-    public static Comparable findMaximumBetween3Values(ArrayList myNumList) {
-        // To find max from the arraylist
-        return Collections.max(myNumList);
+public class FindMaximum { // Find maximum Integer value from the given list
+    public static int findMaximumBetween3Integer(int[] arrayInt) {
+        int max = arrayInt[0];
+        for(int index = 1; index < arrayInt.length; index++) {
+            if (arrayInt[index] > max)
+                max = arrayInt[index];
+        }
+        return max;
+    }
+    // Find maximum Float value from the given list
+    public static float findMaximumBetween3FloatValues(float[] arrayFloat) {
+        float max = arrayFloat[0];
+        for(int index = 1; index < arrayFloat.length; index++) {
+            if (arrayFloat[index] > max)
+                max = arrayFloat[index];
+        }
+        return max;
+    }
+ // Find maximum String value from the given list
+    public static String findMaximumBetween3Strings(String[] arrayString) {
+        String max = arrayString[0];
+        for(int index = 1; index < arrayString.length; index++) {
+            if (arrayString[index].compareTo(max) > 0)
+                max = arrayString[index];
+        }
+        return max;
+    }
+    //To print the Integer values
+    public static void toPrint(int[] integerArray) {
+        for(int index = 0; index < integerArray.length; index++) {
+            System.out.print(integerArray[index] + "  ");
+        }
+    }
+    //To print the Float values
+    public static void toPrint(float[] integerArray) {
+        for(int index = 0; index < integerArray.length; index++) {
+            System.out.print(integerArray[index] + "  ");
+        }
+    }
+    //To print the String values
+    public static void toPrint(String[] integerArray) {
+        for(int index = 0; index < integerArray.length; index++) {
+            System.out.print(integerArray[index] + "  ");
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Test maximum Program");
+        int[] arrayInt = {10,5,9};
+        float[] arrayFloat = {10,5,9};
+        String[] arrayString = {"Banana","Apple","Peach"};
+        toPrint(arrayInt);
+        System.out.println("The maximum valaue of the  array is " + findMaximumBetween3Integer(arrayInt));
+        toPrint(arrayFloat);
+        System.out.println("The maximum valaue of the  array is " + findMaximumBetween3FloatValues(arrayFloat));
+        toPrint(arrayString);
+        System.out.println("The maximum valaue of the  array is " + findMaximumBetween3Strings(arrayString));
     }
-}
+}    
--- FindMaximumTest.java
+++ FindMaximumTest.java
@@ -1,83 +1,61 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 
 public class FindMaximumTest {
     static FindMaximum findMax = new FindMaximum();
-    static ArrayList<Integer> myIntegerList =  new ArrayList<>();
-    static ArrayList<Float> myFloatList = new ArrayList<>();
-    static ArrayList<String> myStringList = new ArrayList<>();
     @Test
     //Given 3 numbers First Number is Maximum
     public void given3Numbers1stIsMaximum() {
-        myIntegerList.add(10);//Adding the values to the arraylist
-        myIntegerList.add(5);
-        myIntegerList.add(9);
-        Assert.assertEquals(10, findMax.findMaximumBetween3Values(myIntegerList));
+        int[] arrayInt = {10,5,9};
+        Assert.assertEquals(10, findMax.findMaximumBetween3Integer(arrayInt));
     }
     @Test
     //Given 3 numbers Second Number is Maximum
     public void given3Numbers2ndIsMaximum() {
-        myIntegerList.add(5);
-        myIntegerList.add(10);
-        myIntegerList.add(9);
-        Assert.assertEquals(10, findMax.findMaximumBetween3Values(myIntegerList));
+        int[] arrayInt = {5,10,9};
+        Assert.assertEquals(10, findMax.findMaximumBetween3Integer(arrayInt));
     }
     @Test
     //Given 3 numbers Third Number is Maximum
     public void given3Numbers3rdIsMaximum() {
-        myIntegerList.add(5);
-        myIntegerList.add(9);
-        myIntegerList.add(10);
-        Assert.assertEquals(10, findMax.findMaximumBetween3Values(myIntegerList));
+        int[] arrayInt = {9,5,10};
+        Assert.assertEquals(10, findMax.findMaximumBetween3Integer(arrayInt));
     }
     @Test
     //Given 3 float values First float values is Maximum
     public void given3FloatValue1stIsMaximum() {
-        myFloatList.add(10f);//Adding the values to the arraylist
-        myFloatList.add(5f);
-        myFloatList.add(9f);
-        Assert.assertEquals(10f, findMax.findMaximumBetween3Values(myFloatList));
+        float[] arrayFloat = {10, 5, 9};
+        Assert.assertEquals(10, findMax.findMaximumBetween3FloatValues(arrayFloat), 0);
     }
     @Test
     //Given 3 float values Second float values is Maximum
     public void given3FloatValue2ndIsMaximum() {
-        myFloatList.add(5f);
-        myFloatList.add(10f);
-        myFloatList.add(9f);
-        Assert.assertEquals(10f, findMax.findMaximumBetween3Values(myFloatList));
+        float[] arrayFloat = {5,10,9};
+        Assert.assertEquals(10, findMax.findMaximumBetween3FloatValues(arrayFloat), 0);
     }
     @Test
     //Given 3 float values  Third float values is Maximum
     public void given3FloatValue3rdIsMaximum() {
-        myFloatList.add(5f);
-        myFloatList.add(9f);
-        myFloatList.add(10f);
-        Assert.assertEquals(10f, findMax.findMaximumBetween3Values(myFloatList));
+        float[] arrayFloat = {9,5,10};
+        Assert.assertEquals(10, findMax.findMaximumBetween3FloatValues(arrayFloat), 0);
     }
     @Test
-    //Given 3 numbers First numbers is Maximum
+    //Given 3 String values First String values is Maximum
     public void given3String1stIsMaximum() {
-        myStringList.add("Peach"); //Adding the values to the arraylist
-        myStringList.add("Apple");
-        myStringList.add("Banana");
-        Assert.assertEquals("Peach", findMax.findMaximumBetween3Values(myStringList));
+        String[] arrayString = {"Peach","Banana","Apple"};
+        Assert.assertEquals("Peach", findMax.findMaximumBetween3Strings(arrayString));
     }
     @Test
-    //Given 3 string Second string is Maximum
+    //Given 3 String values Second String values is Maximum
     public void given3String2ndIsMaximum() {
-        myStringList.add("Apple");
-        myStringList.add("Peach");
-        myStringList.add("Banana");
-        Assert.assertEquals("Peach", findMax.findMaximumBetween3Values(myStringList));
+        String[] arrayString = {"Banana","Peach","Apple"};
+        Assert.assertEquals("Peach", findMax.findMaximumBetween3Strings(arrayString));
     }
     @Test
-    //Given 3 String Third String is Maximum
+    //Given 3 String values  Third String values is Maximum
     public void given3String3rdIsMaximum() {
-        myStringList.add("Banana");
-        myStringList.add("Apple");
-        myStringList.add("Peach");
-        Assert.assertEquals("Peach", findMax.findMaximumBetween3Values(myStringList));
+        String[] arrayString = {"Banana","Apple","Peach"};
+        Assert.assertEquals("Peach", findMax.findMaximumBetween3Strings(arrayString));
     }
 }
