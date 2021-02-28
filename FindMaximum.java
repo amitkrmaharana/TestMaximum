5a341666076be8aa686b434da6069a90b012d11f
amitkrmaharana <ak657harley@gmail.com>
2021-02-28 20:08:00 +0530

    Refactored: to find max for 3 Float values

--- FindMaximum.java
+++ FindMaximum.java
@@ -1,12 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-public class FindMaximum { // Find maximum number from the given list
-    public static Comparable findMaximumBetween3Values(ArrayList myNumList) {
-        // To find max from the arraylist
-        return Collections.max(myNumList);
+public class FindMaximum { // Find maximum Integer value from the given list
+    public static int findMaximumBetween3Integer(int[] arrayInt) {
+        int max = arrayInt[0];
+        for(int index = 1; index < arrayInt.length; index++) {
+            if (arrayInt[index] > max)
+                max = arrayInt[index];
+        }
+        return max;
     }
-    public static void main(String[] args) {
-        System.out.println("Welcome to Test maximum Program");
+    // Find maximum Float value from the given list
+    public static float findMaximumBetween3FloatValues(float[] arrayFloat) {
+        float max = arrayFloat[0];
+        for(int index = 1; index < arrayFloat.length; index++) {
+            if (arrayFloat[index] > max)
+                max = arrayFloat[index];
+        }
+        return max;
     }
-}
+}    
